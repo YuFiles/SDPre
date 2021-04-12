5e753bb36128a673d18f4b9c51fc9e79d5267cc0
Brandon Files <files.brandon@gmail.com>
2021-04-11 19:57:26 -0500

    Completed all the objective3 exercises

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,13 @@
+public class Objective3Lab2 {
+  public static void main(String[] args){
+    double side1, side2, hypotenuse;
+    side1 = 10;
+    side2 = 8;
+    hypotenuse = Math.sqrt(Math.pow(side1,2) + Math.pow(side2,2));
+    System.out.println("The hypotenuse of a triangle with sides " + side1 + " and " + side2 + " is " + hypotenuse);
+
+    }
+
+
+
+}
---
+++ Labs/Objective3Lab3.java
@@ -0,0 +1,11 @@
+public class Objective3Lab3{
+  public static void main(String[] args){
+    int first, second, third;
+    first = 1 + (int)(Math.random() * 6);
+    second = 1 + (int)(Math.random() * 6);
+    third = 1 + (int)(Math.random() * 6);
+
+    System.out.println(first +"+"+ second +"+"+ third +"="+ (int)(first+second+third));
+
+  }
+}
