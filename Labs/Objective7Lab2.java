e81d915a38ac4a6c16e503c9726ebe2751f2f35b
Yushin <Files.Brandon@Gmail.com>
2021-04-16 15:12:41 +0000

    Lab 2 time

---
+++ Labs/Objective7Lab2.java
@@ -0,0 +1,13 @@
+public class Objective7Lab2 {
+  public static void main(String[] args) {
+  int counter = 0;
+
+    while(counter <= 20) {
+      /*
+      Your solution goes here
+      */
+			System.out.println(counter);
+			counter+=1;
+    }
+  }
+}
